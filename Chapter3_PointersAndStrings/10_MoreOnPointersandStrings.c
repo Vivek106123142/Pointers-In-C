5dcf2a995e195343294f0828bd6c3882ed94e657
Vivek Kumar <kmrvivek@gmail.com>
2023-11-15 07:22:06 +0530

    idk what check const pointer

--- Chapter3_PointersAndStrings/10_MoreOnPointersandStrings.c
+++ Chapter3_PointersAndStrings/10_MoreOnPointersandStrings.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     char str1[]="hello";
     char *p="hello";
+    printf("%s\n",p);
     //str1="bye";  THIS GIVES ERROR
     p="bye";
+    printf("%s",p);
     return 0;
 }
--- Chapter3_PointersAndStrings/12_ConstPointers.c
+++ Chapter3_PointersAndStrings/12_ConstPointers.c
@@ -1,24 +1,52 @@
+// #include<stdio.h>
+// void xstrcpy(char*,char*);
+// int main()
+// {
+//     char str1[]="nagpur";
+//     char str2[50];
+//     xstrcpy(str2,str1);
+//     printf("%s\n",str2);
+//     return 0;
+// }
+// void xstrcpy(char*t,char*s)
+// {
+//     while(*s !='\0')
+//     {
+//         *t=*s;
+//         t++;
+//         s++;
+//     }
+    
+//     *t='\0';
+//     s=s-6;
+//     *s='K';
+    
+// }
+
+
+
 #include<stdio.h>
-void xstrcpy(char*,char*);
-int main()
-{
-    char str1[]="nagpur";
-    char str2[10];
-    xstrcpy(str2,str1);
-    printf("%s\n",str2);
+
+void xstrcpy(char*, char*);
+
+int main() {
+    char str1[] = "nagpur";
+    char str2[50];
+    
+    // Modify the original string before copying
+    str1[1] = 'K';
+
+    xstrcpy(str2, str1);
+    printf("%s\n", str2);
     return 0;
 }
-void xstrcpy(char*t,char*s)
-{
-    while(*s !='\0')
-    {
-        *t=*s;
+
+void xstrcpy(char *t, char *s) {
+    while (*s != '\0') {
+        *t = *s;
         t++;
         s++;
     }
-    
-    *t='\0';
-    s=s-6;
-    *s='k';
-    
+
+    *t = '\0';
 }
