cae1b70146063bf595e2b8d408d1573dca265959
Vivek Kumar <kmrvivek@gmail.com>
2023-11-26 15:45:56 +0530

    addatbeg,display,addafter

--- DataStructures/1_LinkedListAppend.c
+++ DataStructures/1_LinkedListAppend.c
@@ -6,6 +6,10 @@ struct node
     struct node* link;
 };
 void append(struct node**,int);
+void addatbeg(struct node**,int);
+void addafter(struct node*,int,int);
+void display(struct node*q);
+int count(struct node *q);
 int main()
 {
   struct node *p; //first node always points to first node
@@ -15,6 +19,16 @@ int main()
   append(&p,3);
   append(&p,4);
   append(&p,17);
+  display(p);
+  addatbeg(&p,999);
+  addatbeg(&p,888);
+  addatbeg(&p,777);
+  display(p);
+  addafter(p,7,0);
+  addafter(p,2,1);
+  addafter(p,1,99);
+  display(p);
+  printf("\nnumber of elements in LL:%d\n",count(p));
 return 0;
 }
 
@@ -44,3 +58,67 @@ void append(struct node **q,int num)
         temp->link=r;
     }
 }
+
+//ADDING NEW NODE AT BEGINNING OF LL
+
+void addatbeg(struct node**q,int num)
+{
+    struct node* temp;
+    //adding new node
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=num;
+    temp->link=*q;
+    *q=temp;
+}
+
+//ADDING NEW NODE AFTER SPECIFIED NUMBER OF NODES
+
+void addafter(struct node *q,int loc,int num)
+{
+    struct node *temp,*r;
+    int i;
+    temp=q;
+    //skipping desired portion
+    for(i=0;i<loc;i++)
+    {
+        temp=temp->link;
+        //if end of LL encountered
+        if(temp==NULL)
+        {
+            printf("There are less than %d elements in list\n",loc);
+            return;
+        }
+    }
+    //INSERT NEW NODE
+    r=(struct node*)malloc(sizeof(struct node));
+    r->data=num;
+    r->link=temp->link;
+    temp->link=r;
+}
+
+//DISPLAY CONTENTS OF LL
+
+void display(struct node*q)
+{
+    printf("\n");
+    //TRAVERSE ENTIER LL
+    while(q!=NULL)
+    {
+        printf("%d\t",q->data);
+        q=q->link;
+    }
+}
+
+//COUNTING NO. OF NODES IN LL
+
+int count(struct node *q)
+{
+    int c=0;
+    //TRAVERSE LL
+    while(q!=NULL)
+    {
+        q=q->link;
+        c++;
+    }
+    return c;
+} 
